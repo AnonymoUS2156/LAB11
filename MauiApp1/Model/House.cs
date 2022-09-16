d7020d1e96a3a45fd90a0527b80ff83b2197e201
323 <323@323-12>
2022-09-16 11:34:07 +0300

    Добавьте файлы проекта.

---
+++ MauiApp1/Model/House.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui.Controls.Platform.Compatibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Model
+{
+    internal class House
+    {
+        public int Id { get; set; }
+        public string Name{ get; set; }
+        public string Adress{ get; set; }
+        public string Price { get; set; }
+    }
+}
